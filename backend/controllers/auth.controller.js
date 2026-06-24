bfa489125ebac1c56d21ada559c3521d6330b949
Mariam Oduye <oduyemariam@gmail.com>
2026-06-24 15:10:00 +0100

    fix token

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -72,7 +72,8 @@ export const login = async (req, res) =>{
         if(!user || !isPasswordCorrect){
             return res.status(400).json({error: "Invalid username or password"})
         }
-
+        
+        generateTokenAndSetCookie(user._id, res);
         const token = generateTokenAndSetCookie(newUser._id, res);
 
         res.status(200).json({
